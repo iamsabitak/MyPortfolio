3099b72339893d74718ee624ea8bc0f2f4eb84f9
sabita <sabitakhadka091@gmail.com>
2023-12-20 18:14:12 +0545

    src/Navbar.jsx

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import { Link } from "react-router-dom";
+function Navbar() {
+  return (
+    <nav className="navbar" id="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
