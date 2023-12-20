b98addf2f13eb1920ba985c11856097e6f990af7
sabita <sabitakhadka091@gmail.com>
2023-12-20 18:19:29 +0545

    Added Projects component displaying project previews

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import './index.css';
+import React from "react";
+
+import "./index.css";
+
+import Header from "./Header";
+import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./NotFound";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
