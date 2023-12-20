d1950d6856429f6bd0fa709cc63dc38efdbcd84a
sabita <sabitakhadka091@gmail.com>
2023-12-20 18:14:55 +0545

    Added NotFound component for 404 error handling

---
+++ src/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>404 - Not Found</h1>
+      <p>Oops! The page you're looking for doesn't exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
