10d35287de4e5e377c20a2f15b3850d6f4c039eb
sabita <sabitakhadka091@gmail.com>
2024-01-02 16:34:01 +0545

    feat: Add project details to SectionProjects component

--- src/projects/SectionProjects.jsx
+++ src/projects/SectionProjects.jsx
@@ -22,30 +22,12 @@ function SectionProjects({ ProjectsImg }) {
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias aliquam voluptatum velit? Illo ullam consectetur beatae ea commodi earum ab!"
           link="https://usepopcorrrn.netlify.app"
         />
-        <ProjectsImg
+        {/* <ProjectsImg
           src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210712212601/Top-10-Front-End-Web-Development-Projects-for-Beginners.png"
           projectTitle="Eact-n-Split"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias aliquam voluptatum velit? Illo ullam consectetur beatae ea commodi earum ab!"
           link="https://zingy-arithmetic-fbd779.netlify.app"
-        />
-        <ProjectsImg
-          src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210712212601/Top-10-Front-End-Web-Development-Projects-for-Beginners.png"
-          projectTitle="UsePopCorn"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias aliquam voluptatum velit? Illo ullam consectetur beatae ea commodi earum ab!"
-          link="https://usepopcorrrn.netlify.app"
-        />
-        <ProjectsImg
-          src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210712212601/Top-10-Front-End-Web-Development-Projects-for-Beginners.png"
-          projectTitle="Eact-n-Split"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias aliquam voluptatum velit? Illo ullam consectetur beatae ea commodi earum ab!"
-          link="https://zingy-arithmetic-fbd779.netlify.app"
-        />
-        <ProjectsImg
-          src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210712212601/Top-10-Front-End-Web-Development-Projects-for-Beginners.png"
-          projectTitle="UsePopCorn"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias aliquam voluptatum velit? Illo ullam consectetur beatae ea commodi earum ab!"
-          link="https://usepopcorrrn.netlify.app"
-        />{" "}
+        /> */}
       </div>
     </div>
   );
