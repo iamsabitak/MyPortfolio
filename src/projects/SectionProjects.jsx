a48fcb0fd55f605298b9b61d7371ab393848c9c5
sabita <sabitakhadka091@gmail.com>
2024-01-03 16:18:30 +0545

    implemented the forecastify app link

--- src/projects/SectionProjects.jsx
+++ src/projects/SectionProjects.jsx
@@ -10,24 +10,18 @@ function SectionProjects({ ProjectsImg }) {
     <div className="container">
       <h2>Projects</h2>
       <div className="projects-grid">
-        <ProjectsImg
-          src="https://miro.medium.com/v2/resize:fit:1400/1*lwyiHBfRgsLCDAz1e9TF2g.png"
-          projectTitle="Eact-n-Split"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias aliquam voluptatum velit? Illo ullam consectetur beatae ea commodi earum ab!"
-          link="https://zingy-arithmetic-fbd779.netlify.app"
-        />
-        <ProjectsImg
+        {/* <ProjectsImg
           src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210712212601/Top-10-Front-End-Web-Development-Projects-for-Beginners.png"
           projectTitle="UsePopCorn"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias aliquam voluptatum velit? Illo ullam consectetur beatae ea commodi earum ab!"
           link="https://usepopcorrrn.netlify.app"
-        />
-        {/* <ProjectsImg
-          src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210712212601/Top-10-Front-End-Web-Development-Projects-for-Beginners.png"
-          projectTitle="Eact-n-Split"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias aliquam voluptatum velit? Illo ullam consectetur beatae ea commodi earum ab!"
-          link="https://zingy-arithmetic-fbd779.netlify.app"
         /> */}
+        <ProjectsImg
+          src="https://www.shutterstock.com/image-vector/weather-forecast-app-interface-vector-260nw-2045868041.jpg"
+          projectTitle="ForeCastify"
+          text="A simple weather application built with React that enables users to check the weather forecast for specific locations."
+          link="https://659531ff0f70d883df99e866--tranquil-halva-ce5a42.netlify.app"
+        />
       </div>
     </div>
   );
