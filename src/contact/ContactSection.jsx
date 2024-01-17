f17fe91622a1ecc42492777cb54958beab1ad630
sabita <sabitakhadka091@gmail.com>
2024-01-17 10:15:41 +0545

    proprly import the components  from and contacticons

--- src/contact/ContactSection.jsx
+++ src/contact/ContactSection.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
+import Form from "./Form";
+import ContactIcons from "./ContactIcons";
 
-function ContactSection({Form,ContactIcons}) {
+function ContactSection() {
   return (
     <div className="container">
-        <h2>Contact Me</h2>
-        <p>
-          Let's connect! Use the form below or reach out to me at{" "}
-          <a href="mailto:sabitakhadka091@gmail.com">
-            sabitakhadka091@gmail.com
-          </a>
-          . Let's work together and bring your ideas to life!
-        </p>
-        <Form />
-        <ContactIcons />
-      </div>
-  )
+      <h2>Contact Me</h2>
+      <p>
+        Let's connect! Use the form below or reach out to me at{" "}
+        <a href="mailto:sabitakhadka091@gmail.com">sabitakhadka091@gmail.com</a>
+        . Let's work together and bring your ideas to life!
+      </p>
+      <Form />
+      <ContactIcons />
+    </div>
+  );
 }
 
-export default ContactSection
+export default ContactSection;
